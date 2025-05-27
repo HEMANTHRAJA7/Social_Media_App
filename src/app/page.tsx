cda81583e33ad4e1fd3bdbbaf9db907fa2cceba8
Hemanth Raja <hemanthraja2004@gmail.com>
2025-05-27 11:39:52 +0530

    Clerk setup and shadcn setup

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,9 +1,34 @@
+import ModeToggle from "@/components/ModeToggle";
+import { Button } from "@/components/ui/button";
+import {
+  SignedIn,
+  SignedOut,
+  SignInButton,
+  SignUpButton,
+  UserButton,
+} from "@clerk/nextjs";
 import Image from "next/image";
 
 export default function Home() {
   return (
     <div>
-      <h1>hello</h1>
+      <SignedOut>
+        <SignInButton mode="modal">
+          
+          <Button>
+            Sign In
+          </Button>
+        </SignInButton>
+        <SignUpButton mode="modal">
+          <Button variant={"secondary"}>
+            Sign Up
+          </Button>
+        </SignUpButton>
+      </SignedOut>
+      <SignedIn>
+        <UserButton />
+      </SignedIn>
+      <ModeToggle />
     </div>
   );
 }
